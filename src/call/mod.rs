3cc53579ec9991a9c4f52b4ed079476a4050593f
shadowWalker <hieu811@gmail.com>
2024-06-28 21:24:56 +0700

    add flow for connect call

--- src/call/mod.rs
+++ src/call/mod.rs
@@ -6,11 +6,7 @@ use tokio::{net::UdpSocket, select, sync::mpsc::Sender};
 use tokio_util::task::TaskTracker;
 
 use crate::{MainEvent, NgCommand};
-use std::{
-  borrow::BorrowMut,
-  collections::{HashMap, VecDeque},
-  sync::Arc,
-};
+use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug)]
 pub enum CallMsg {
@@ -36,52 +32,50 @@ pub struct CallLeg {
   pub local_sdp: String,
   pub rtp_port: isize,
   pub rtcp_port: isize,
-  internal_tx: tokio::sync::mpsc::Sender<Vec<u8>>,
-  internal_rx: tokio::sync::mpsc::Receiver<Vec<u8>>,
 }
 
 impl CallLeg {
   pub fn new(id: String, remote_sdp: String, local_sdp: String, rtp_port: isize, rtcp_port: isize) -> Self {
-    let (tx, rx) = tokio::sync::mpsc::channel(100);
     CallLeg {
       id,
       remote_sdp,
       local_sdp,
       rtcp_port,
       rtp_port,
-      internal_tx: tx,
-      internal_rx: rx,
     }
   }
 
-  pub async fn process(&mut self, call_chan: tokio::sync::mpsc::Sender<CallEvent>) {
-    let mut cur_addr = None;
-    let mut buf = vec![0; 1400];
+  pub fn process(&self, call_chan: tokio::sync::mpsc::Sender<CallEvent>, tracker: &mut TaskTracker) -> Sender<Vec<u8>> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<Vec<u8>>(100);
+
     let id = self.id.clone();
-    let socket = UdpSocket::bind(format!("0.0.0.0:{}", self.rtp_port)).await.unwrap();
-    loop {
-      select! {
-        Ok((len, addr)) = socket.recv_from(&mut buf) => {
-          if cur_addr.is_none() {
-            cur_addr = Some(addr.clone());
+    let rtp_port = self.rtp_port;
+
+    tracker.spawn(async move {
+      let socket = UdpSocket::bind(format!("0.0.0.0:{}", rtp_port)).await.unwrap();
+      let mut cur_addr = None;
+      let mut buf = vec![0; 1400];
+      loop {
+        select! {
+          Ok((n, addr)) = socket.recv_from(&mut buf) => {
+            if cur_addr.is_none() {
+              cur_addr = Some(addr);
+            }
+            let data = buf[..n].to_vec();
+            call_chan.send(CallEvent::OnLegData(id.clone(), data)).await.unwrap();
           }
-          let data = buf[..len].to_vec();
-          call_chan.send(CallEvent::OnLegData(id.clone(), data)).await.unwrap();
-        }
-        Some(data) = self.internal_rx.recv() => {
-          if let Some(addr) = cur_addr {
-            socket.send_to(&data, addr).await.unwrap();
+          Some(data) = rx.recv() => {
+            if let Some(addr) = cur_addr {
+              socket.send_to(&data, addr).await.unwrap();
+            }
           }
-        }
-        else => {
-          break;
+          else => {
+            break;
+          }
         }
       }
-    }
-  }
-
-  pub fn get_sender(&self) -> tokio::sync::mpsc::Sender<Vec<u8>> {
-    self.internal_tx.clone()
+    });
+    tx
   }
 }
 
@@ -132,26 +126,24 @@ impl Call {
     self.call_id.clone()
   }
 
-  pub async fn add_leg(&mut self, leg: Arc<CallLeg>) {
-    // self.legs.insert(leg.id.clone(), leg);
-    let leg_tx = leg.get_sender();
-    let call_tx = self.internal_tx.clone();
-    self
-      .internal_tx
-      .send(CallEvent::NewLeg(leg.id.clone(), leg_tx))
-      .await
-      .unwrap();
-    self.task_tracker.spawn(async move {
-      leg.borrow_mut().process(call_tx).await;
-    });
+  pub async fn add_leg(&mut self, leg: CallLeg) {
+    let leg_id = leg.id.clone();
+    let leg_tx = leg.process(self.internal_tx.clone(), &mut self.task_tracker);
+    self.legs.insert(leg_id.clone(), leg);
+    self.internal_tx.send(CallEvent::NewLeg(leg_id, leg_tx)).await.unwrap();
   }
 
-  async fn active_leg(&mut self, id: String) {}
-
-  pub async fn delete(&mut self) {
+  pub async fn delete(&mut self) -> Vec<isize> {
     self.task_tracker.close();
     self.task_tracker.wait().await;
-    // self.legs.clear();
+
+    let mut ports = vec![];
+    for (_, leg) in self.legs.iter() {
+      ports.push(leg.rtp_port);
+      ports.push(leg.rtcp_port);
+    }
+    self.legs.clear();
+    ports
   }
 }
 
@@ -217,9 +209,9 @@ impl CallManager {
                 rtp_port,
                 origin: src_sdp_obj.origin,
               });
-              let leg = Arc::new(CallLeg::new(from_tag, remote_sdp.clone(), sdp, rtp_port, rtcp_port));
+              let leg = CallLeg::new(from_tag, remote_sdp.clone(), sdp, rtp_port, rtcp_port);
               let mut call = Call::new(call_id.clone());
-              call.add_leg(leg);
+              call.add_leg(leg).await;
               self.call_map.insert(call_id.clone(), call);
 
               let result = CallResult::Offer(id, remote_sdp);
@@ -246,24 +238,66 @@ impl CallManager {
             ice,
           } => {
             if let Some(call) = self.call_map.get_mut(&call_id) {
-              let remote_sdp = sdp.clone();
-              // let leg = CallLeg::new(to_tag, remote_sdp.clone(), sdp);
-              // call.add_leg(leg);
-              let result = CallResult::Answer(id, remote_sdp);
+              match SessionDescription::try_from(sdp.clone()) {
+                Ok(src_sdp_obj) => {
+                  if self.port_queue.len() < 2 {
+                    self
+                      .out_chan
+                      .send(MainEvent::ActionResult(CallActionResult::Error(
+                        id,
+                        "Not enough port".to_string(),
+                      )))
+                      .await
+                      .unwrap();
+                    return;
+                  }
+                  let rtp_port = self.port_queue.pop_back().unwrap();
+                  let rtcp_port = self.port_queue.pop_back().unwrap();
+                  let remote_sdp = generate_sdp(SdpConfig {
+                    addr: self.cfg.addr.clone(),
+                    rtcp_port,
+                    rtp_port,
+                    origin: src_sdp_obj.origin,
+                  });
+                  let leg = CallLeg::new(from_tag, remote_sdp.clone(), sdp, rtp_port, rtcp_port);
+                  call.add_leg(leg).await;
+                  let result = CallResult::Answer(id, remote_sdp);
+                  self
+                    .out_chan
+                    .send(MainEvent::ActionResult(CallActionResult::Ok(result)))
+                    .await
+                    .unwrap();
+                }
+                Err(e) => {
+                  self
+                    .out_chan
+                    .send(MainEvent::ActionResult(CallActionResult::Error(id, e.to_string())))
+                    .await
+                    .unwrap();
+                  return;
+                }
+              }
+            } else {
               self
                 .out_chan
-                .send(MainEvent::ActionResult(CallActionResult::Ok(result)))
+                .send(MainEvent::ActionResult(CallActionResult::Error(
+                  id,
+                  "Call not found".to_string(),
+                )))
                 .await
                 .unwrap();
             }
           }
           NgCommand::Delete {
             call_id,
-            from_tag,
-            to_tag,
+            from_tag: _,
+            to_tag: _,
           } => {
             if let Some(mut call) = self.call_map.remove(&call_id) {
-              call.delete().await;
+              let relased_ports = call.delete().await;
+              for port in relased_ports {
+                self.port_queue.push_front(port);
+              }
               let result = CallResult::Delete(id);
               self
                 .out_chan
